7b5f6a03417b080d109ebbc4bf47f0f378143f96
Eric Herot <eric.github@herot.com>
2015-04-13 13:44:23 -0400

    Switch to Apache 2.0 license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'et_hostname'
 maintainer       'Jeff Byrnes'
 maintainer_email 'devops@evertrue.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Set the hostname and put sane values in the hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
