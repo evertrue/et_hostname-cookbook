6179c00ffdc759132dbfdc63a0fe56e8c1ac49a7
Eric Herot <eric.github@herot.com>
2015-04-13 13:30:29 -0400

    Improve the description

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'et_hostname'
 maintainer       'Jeff Byrnes'
 maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
-description      'Installs/Configures hostname'
+description      'Set the hostname and put sane values in the hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
